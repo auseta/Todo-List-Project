ff282e98727ae9225c32292831701b50c6ab38c0
Augusto Seta <alseta97@gmail.com>
2022-11-09 15:59:58 -0300

    webpack.config was modified

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: 'asset/resource',
-    },
-   ],
- },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
 };
