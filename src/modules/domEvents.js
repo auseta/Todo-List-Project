ac43c858bc596e1fa18188131c7c6a27fe4a1600
Augusto Seta <alseta97@gmail.com>
2022-11-09 13:33:13 -0300

    minor changes was added

--- src/modules/domEvents.js
+++ src/modules/domEvents.js
@@ -3,5 +3,6 @@ import { openInbox } from "./manipulation";
 const d = document;
 const inboxSection = d.querySelector('#inbox');
 inboxSection.addEventListener('click', () => {
+  inboxSection.style.cssText = 'background-color: #03a062; color: #000000'
   openInbox()
 })
