2e8b0f274ea23b2ccdee30f3e0227ea7577ae945
Augusto Seta <alseta97@gmail.com>
2022-11-09 23:57:23 -0300

    events added to the Today section

--- src/modules/domEvents.js
+++ src/modules/domEvents.js
@@ -1,8 +1,17 @@
-import { openInbox } from "./manipulation";
+import { openInbox, openToday } from "./manipulation";
 
 const d = document;
+
 const inboxSection = d.querySelector('#inbox');
 inboxSection.addEventListener('click', () => {
-  inboxSection.style.cssText = 'background-color: #03a062; color: #000000'
+  todaySection.style.cssText = '';
+  inboxSection.style.cssText = 'background-color: #03a062c6; color: #000000;'
   openInbox()
+})
+
+const todaySection = d.querySelector('#today');
+todaySection.addEventListener('click', () => {
+  inboxSection.style.cssText = '';
+  todaySection.style.cssText = 'background-color: #03a062c6; color: #000000;'
+  openToday()
 })
