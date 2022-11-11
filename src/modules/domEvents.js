c66e2ca773058440e85422dfbe30ec43504f6162
Augusto Seta <alseta97@gmail.com>
2022-11-10 22:57:32 -0300

    Events have been added to the today and week sections. Events have started to be added to the projects section.

--- src/modules/domEvents.js
+++ src/modules/domEvents.js
@@ -1,10 +1,12 @@
-import { openInbox, openToday } from "./manipulation";
+import { openInbox, openToday, openThisWeek } from "./manipulation";
+import { addProject } from "./project";
 
 const d = document;
 
 const inboxSection = d.querySelector('#inbox');
 inboxSection.addEventListener('click', () => {
   todaySection.style.cssText = '';
+  weekSection.style.cssText = '';
   inboxSection.style.cssText = 'background-color: #03a062c6; color: #000000;'
   openInbox()
 })
@@ -12,6 +14,30 @@ inboxSection.addEventListener('click', () => {
 const todaySection = d.querySelector('#today');
 todaySection.addEventListener('click', () => {
   inboxSection.style.cssText = '';
+  weekSection.style.cssText = '';
   todaySection.style.cssText = 'background-color: #03a062c6; color: #000000;'
   openToday()
+})
+
+const weekSection = d.querySelector('#week');
+weekSection.addEventListener('click', () => {
+  inboxSection.style.cssText = '';
+  todaySection.style.cssText = '';
+  weekSection.style.cssText = 'background-color: #03a062c6; color: #000000;'
+  openThisWeek()
+})
+
+const inputProjectName = d.querySelector('#input-project-name');
+const buttonAddProject = d.querySelector('#add-project-button');
+inputProjectName.addEventListener('click', () => {
+  inboxSection.style.cssText = '';
+  weekSection.style.cssText = '';
+  todaySection.style.cssText = '';
+})
+
+buttonAddProject.addEventListener('click', () => {
+  inboxSection.style.cssText = '';
+  weekSection.style.cssText = '';
+  todaySection.style.cssText = '';
+  inputProjectName.value ? addProject() : console.log('chau')
 })
