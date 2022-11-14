d4dceb19d8834c5b06932fc5bed19ac17538d7fe
Augusto Seta <alseta97@gmail.com>
2022-11-14 14:46:19 -0300

    added conditions to create a project

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,9 +1,15 @@
-import { Project, projectsStorage } from "./task";
+import { Project, projectsStorage } from "./classes";
 
 const d = document;
 const inputProjectName = d.querySelector('#input-project-name');
 const projectsContainer = d.querySelector('.projects')
 const content = d.querySelector('.content');
+
+const closeModalError = () => {
+  const modalErrorContainer = d.querySelector('.modal-error');
+  const buttonCloseModal = d.querySelector('.close-modal');
+  buttonCloseModal.addEventListener('click', () => modalErrorContainer.remove())
+}
 
 export const displayModalError = () => {
   const modalErrorContainer = d.createElement('div');
@@ -16,7 +22,8 @@ export const displayModalError = () => {
       <p>{ ERROR, THE PROJECT ENTERED ALREADY EXISTS }</p>
     </div>
   `
-  content.append(modalErrorContainer)
+  content.append(modalErrorContainer);
+  closeModalError()
 }
 
 export const createProject = () => {
@@ -24,7 +31,7 @@ export const createProject = () => {
   if (projectsStorage.length === 0 || !projectsStorage.some( project => project.name === projectName )) {
     Project.saveProject(projectName);
     inputProjectName.value = ''
-    addProject()
+    addProject();
   } else {
     displayModalError()
   }
@@ -37,9 +44,13 @@ export const addProject = () => {
     projectContainer.className = 'project';
     projectContainer.id = project.name.split(' ').join('-')
     projectContainer.innerHTML = `
-      <a>${project.name}</a>
-      <button id='delete-project'>X</button>
+      <a class='project-name'>${project.name}</a>
+      <button class='delete-project'>X</button>
+      <div class='.tasks-container'></div>
     `
     projectsContainer.appendChild(projectContainer)
   })
+  Project.deleteProject()
+  Project.openProject()
+
 }
